9c389b69132596b053c5c0e4b1c06e8acc40b708
mike oselle <osellezino@outlook.com>
2020-03-02 10:17:11 +0100

    modal fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './Homepage';
+import Donate from './Donate';
 
 class App extends Component {
   render () {
@@ -9,6 +10,7 @@ class App extends Component {
     <Router>
       <Switch>
         <Route exact path = '/' component={Homepage} />
+        <Route exact path = '/donate' component={Donate}/>
       </Switch>
     </Router>
   );
